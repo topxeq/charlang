2d566215a7d5916af764e18dde9f301a2d3c1c7a
unknown <topget@sina.com>
2024-07-20 13:12:23 +0800

    2024072003

--- cmd/noGui.go
+++ cmd/noGui.go
@@ -9,7 +9,21 @@ import (
 	tk "github.com/topxeq/tkc"
 )
 
+func initGUI() error {
+
+	return fmt.Errorf("GUI not supported.")
+}
+
 func guiHandler(argsA ...interface{}) interface{} {
+	actionA := tk.ToStr(argsA[0])
+
+	// paramsA := argsA[1:]
+
+	switch actionA {
+	case "init":
+		rs := initGUI()
+		return rs
+	}
 	// fmt.Printf("%v\n", "GUI engined disabled")
 	return fmt.Errorf("GUI engined disabled")
 }
